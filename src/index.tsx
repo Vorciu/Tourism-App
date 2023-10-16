ef102bb72a644c0dfa396ac5043af72b1f89dd3b
unknown <michal.rieske@gmail.com>
2023-10-17 00:23:40 +0200

    Added babel,eslint, prettier and basic folder structure

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Home } from "@screens";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home />
+      <Text> This is the home screen of TourismApp</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
